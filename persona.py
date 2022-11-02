a29945d2658d93ecc70d99ed2d7334ecb43c07f5
cristian puerto <capuertoc@correo.udistrital.edu.co>
2022-11-02 17:28:20 -0500

    v 0.001

---
+++ persona.py
@@ -0,0 +1,43 @@
+
+class Persona:
+    
+    def __init__(self):
+        self.nombre=""
+        self.bigote=False
+        self.volumen=""
+        self.gènero=""
+        self.edad=int
+    def gritar(self):
+        pass
+    
+    def bailar(self):
+        pass
+    
+    def saltar():
+        pass
+    
+    
+sujeto_a=Persona()
+
+sujeto_a.nombre="Camilo"
+sujeto_a.bigote=False
+sujeto_a.volumen="alto"
+sujeto_a.edad= 20
+sujeto_a.gènero="pansexual"
+
+    
+sujeto_b=Persona()
+
+sujeto_b.nombre="Cristian"
+sujeto_b.bigote=False
+sujeto_b.volumen="medio"
+sujeto_b.edad= 52
+sujeto_b.gènero="masculino"
+
+sujeto_c=Persona()
+
+sujeto_c.nombre="Lina"
+sujeto_c.bigote=False
+sujeto_c.volumen="bajo"
+sujeto_c.edad= 19
+sujeto_c.gènero="femenino"
